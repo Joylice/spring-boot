175925855e99a1393ab1fd39af8c1eb0a34f2106
Your Name <you@example.com>
2017-06-22 14:01:12 +0800

    Database

--- mybatis/src/main/resources/db/create/core.sql
+++ mybatis/src/main/resources/db/create/core.sql
@@ -1,3 +1,4 @@
+-- 表尾添加的2行 指定数据库引擎，存储格式，如若不然，插入中文汉字，会出现乱码
 -- 用户表
 CREATE TABLE user (
   id                      VARCHAR(32) PRIMARY KEY NOT NULL,
@@ -11,7 +12,9 @@ CREATE TABLE user (
   account_non_locked      BOOLEAN,
   credentials_non_expired BOOLEAN,
   enabled                 BOOLEAN
-);
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 -- 角色表
 CREATE TABLE authority (
   id          VARCHAR(32) PRIMARY KEY NOT NULL,
@@ -20,7 +23,9 @@ CREATE TABLE authority (
   valid_flag  BOOLEAN DEFAULT TRUE,
   sort        BIGINT  DEFAULT 0,
   name        VARCHAR(255)
-);
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- 用户角色关系表
 CREATE TABLE user_authority
@@ -30,7 +35,9 @@ CREATE TABLE user_authority
   CONSTRAINT user_authority_pkey PRIMARY KEY (user_id, authority_id),
   CONSTRAINT user_authority_user_id_fk FOREIGN KEY (user_id) REFERENCES user (id),
   CONSTRAINT user_authority_authority_id_fk FOREIGN KEY (authority_id) REFERENCES authority (id)
-);
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- 菜单表
 CREATE TABLE menu (
@@ -45,13 +52,15 @@ CREATE TABLE menu (
   icon        VARCHAR(255),
   style       VARCHAR(255),
   cls         VARCHAR(255),
-  abstraction    VARCHAR(255),
+  abstraction VARCHAR(255),
   views       VARCHAR(2048),
   active      BOOLEAN DEFAULT FALSE,
   resolve     VARCHAR(255),
   force_hide  BOOLEAN DEFAULT FALSE,
   parent_id   VARCHAR(32)
-);
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE menu_attr (
   id          VARCHAR(32) PRIMARY KEY,
@@ -63,13 +72,17 @@ CREATE TABLE menu_attr (
   value       VARCHAR(255),
   menu_id     VARCHAR(32),
   CONSTRAINT menu_attr_menu_id_fk FOREIGN KEY (menu_id) REFERENCES menu (id)
-);
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
 CREATE TABLE menu_authority (
   menu_id      VARCHAR(32),
   authority_id VARCHAR(255)
-);
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 CREATE TABLE request_map (
   id          VARCHAR(32) PRIMARY KEY NOT NULL,
@@ -79,6 +92,8 @@ CREATE TABLE request_map (
   sort        BIGINT  DEFAULT 0,
   url         VARCHAR(255),
   authority   VARCHAR(255)
-);
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
